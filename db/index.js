338c37e78292df92b70e07991ee1aae3f15bf352
Theo <trgrf1@gmail.com>
2021-09-09 17:26:27 -0400

    saving changes to index.js

--- db/index.js
+++ db/index.js
@@ -13,7 +13,7 @@ class DB {
       // id, first_name, last_name FROM employee TABLE AND department name from department TABLE AND SELECT salary FROM role TABLE
       // YOUR NEED TO USE LEFT JOINS TO JOIN THREE TABLES
       // TODO: YOUR CODE HERE
-      "SELECT employee.id, employee.first_name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department.id, department.name"
+      "SELECT employee.id, employee.first_name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role ON role.department_id = department.id LEFT JOIN department ON department.id GROUP BY employee.id, employee.first_name"
     );
   }
 
@@ -55,7 +55,7 @@ class DB {
       // id, title, salary FROM role TABLE AND department name FROM department TABLE
       // YOU NEED TO USE LEFT JOIN TO JOIN role and department TABLES
       // TODO: YOUR CODE HERE
-      "SELECT role.id, role.title, SUM(role.salary) AS utilized_budget FROM role LEFT JOIN employee ON employee.role_id = role.id LEFT JOIN department ON employee.role_id = role.id GROUP BY role.id, role.title"
+      "SELECT role.id, role.title, SUM(role.salary) AS utilized_budget FROM role LEFT JOIN employee ON employee.role_id = role.id LEFT JOIN department ON department.id GROUP BY role.id, role.title"
     );
   }
 
